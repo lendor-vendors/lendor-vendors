815d4ec2b2127949e2a515344ebe2aac3b15d954
choytr <143041680+choytr@users.noreply.github.com>
2023-11-23 20:23:32 -1000

    Fixed lint errors

--- app/imports/ui/components/ForumCard.jsx
+++ app/imports/ui/components/ForumCard.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Roles } from 'meteor/alanning:roles';
-import { Meteor } from 'meteor/meteor';
-import { Card, Col, Container, Image, Row } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import DeleteButton from './DeleteButton';
 import MiniProfile from './MiniProfile';
 
 /** Renders a single row in the List item table. See pages/YourItems.jsx. */
