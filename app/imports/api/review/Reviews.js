90a30a097a99818797e47108179aa77108c1c69c
marcusp478 <marcusprudencio03@gmail.com>
2023-11-24 18:37:11 -1000

    Created ReviewsCollection and made a publication method for it

---
+++ app/imports/api/review/Reviews.js
@@ -0,0 +1,32 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+/**
+ * The ReviewsCollection. It encapsulates state and variable values for Profile.
+ */
+class ReviewsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ReviewsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      reviewee: String,
+      reviewer: String,
+      rating: { type: Number, min: 0, max: 10 },
+      timeStamp: Date,
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the ReviewsCollection.
+ * @type {ReviewsCollection}
+ */
+export const Reviews = new ReviewsCollection();
