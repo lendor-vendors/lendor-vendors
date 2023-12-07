4b7853ceee41701d70abd33ae1add685cace97e1
Jiawen Chen <jiawenchen213@gmail.com>
2023-12-06 17:37:24 -1000

    changed default data to pass testcafe
    Took 13 minutes

--- app/tests/navbar.component.js
+++ app/tests/navbar.component.js
@@ -13,18 +13,30 @@ class NavBar {
 
   async gotoSignInPage(testController) {
     await this.ensureLogout(testController);
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.click('#login-dropdown');
     await testController.click('#login-dropdown-sign-in');
   }
 
   /** Check that the specified user is currently logged in. */
   async isLoggedIn(testController, username) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     const loggedInUser = Selector('#navbar-current-user').innerText;
     await testController.expect(loggedInUser).eql(username);
   }
 
   /** Check that someone is logged in, then click items to logout. */
   async logout(testController) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.expect(Selector('#navbar-current-user').exists).ok();
     await testController.click('#navbar-current-user');
     await testController.click('#navbar-sign-out');
@@ -33,36 +45,68 @@ class NavBar {
   /** Pull down login menu, go to sign up page. */
   async gotoSignUpPage(testController) {
     await this.ensureLogout(testController);
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.click('#login-dropdown');
     await testController.click('#login-dropdown-sign-up');
   }
 
   /** Go to gallery page after log in. */
   async gotoGalleryPage(testController) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.click('#gallery-item-nav');
   }
 
   async gotoYourItemsPage(testController) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.click('#your-items-nav');
   }
 
   async gotoPostItemPage(testController) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.click('#post-item-nav');
   }
 
   async gotoRequestsPage(testController) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.click('#requests-nav');
   }
 
   async gotoForumsPage(testController) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.click('#forums-nav');
   }
 
   async gotoNotificationsPage(testController) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.click('#notifications-nav');
   }
 
   async gotoViewProfilePage(testController) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
     await testController.click('#navbar-current-user');
     await testController.click('#navbar-view-profile');
   }
