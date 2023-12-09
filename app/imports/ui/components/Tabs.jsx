ed5d5aa66fb679a25138f5702b30004c28e44b6c
Jiawen Chen <jiawenchen213@gmail.com>
2023-12-08 17:18:10 -1000

    updated to also include Pagination in Forums page
    Took 2 hours 55 minutes

--- app/imports/ui/components/Tabs.jsx
+++ app/imports/ui/components/Tabs.jsx
@@ -1,36 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Container, Row } from 'react-bootstrap';
 
-const Tabs = ({ tabNames, sendCurrentTab }) => {
-  const [currentTab, setCurrentTab] = useState(tabNames[0]);
-  return (
-    <Row>
-      <Container className="d-flex justify-content-evenly">
-        {tabNames.map((tabName, index) => (
-          // Button component is temporary. TODO: Re-style tab buttons to look better
-          <Button
-            key={index}
-            onClick={() => {
-              setCurrentTab(tabName);
-              sendCurrentTab(tabName);
-            }}
-            style={{
-              width: '100%',
-              borderRadius: 0,
-            }}
-            variant={currentTab === tabName ? 'success' : 'outline-dark'}
-          >{tabName}
-          </Button>
-        ))}
-      </Container>
-    </Row>
-  );
-};
+const Tabs = ({ tabNames, sendCurrentTab, currentTab }) => (
+  <Row>
+    <Container className="d-flex justify-content-evenly">
+      {tabNames.map((tabName, index) => (
+        // Button component is temporary. TODO: Re-style tab buttons to look better
+        <Button
+          key={index}
+          onClick={() => {
+            sendCurrentTab(tabName);
+          }}
+          style={{
+            width: '100%',
+            borderRadius: 0,
+          }}
+          variant={currentTab === tabName ? 'success' : 'outline-dark'}
+        >{tabName}
+        </Button>
+      ))}
+    </Container>
+  </Row>
+);
 
 Tabs.propTypes = {
   tabNames: PropTypes.arrayOf(PropTypes.string).isRequired,
   sendCurrentTab: PropTypes.func.isRequired,
+  currentTab: PropTypes.string.isRequired,
 };
 
 export default Tabs;
